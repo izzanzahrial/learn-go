37495dbdd0840181e532adc038550830b3e028c7
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2021-09-05 14:05:10 +0700

    add math op, augmented assignment, urnary op, comparison op, boolean op, array and slice

--- basic.go
+++ basic.go
@@ -101,4 +101,175 @@ func main() {
 	fmt.Println(object)
 	fmt.Println(byteObject)
 	fmt.Println(stringObject)
+
+	// Type declarations
+	// make your own type, alias
+	type IdNum int
+	var id IdNum = 12345678
+	fmt.Println(id)
+	fmt.Println(IdNum(87654321))
+
+	// Math operation
+	var a = 4
+	var b = 2
+	var c = a + b
+	fmt.Println(c)
+
+	var d = 20 - 20
+	d = 21 * 2
+	d = 3 / 3
+	d = 10 % 2
+	fmt.Println(d)
+
+	// Augmented assignment
+	var e = 21
+	e += 2 // e = e + 2, the rest is same
+	e -= 2
+	e /= 2
+	e *= 3
+	e %= 2
+	fmt.Println(e)
+
+	// Urnary operation
+	var f = 4
+	f++ // f = f + 1
+	f-- // f = f - 1
+	fmt.Println(f)
+	var negative = -4
+	var positif = +4 // for positif int you dont have to use the "+" symbol
+	fmt.Println(negative)
+	fmt.Println(positif)
+
+	// Comparison operation
+	// same as python
+	// it doesnt have to be int, it can be used for string and else
+	var g = 5
+	var j = 6
+	fmt.Println(g > j)
+	fmt.Println(g < j)
+	fmt.Println(g >= j)
+	fmt.Println(g <= j)
+	fmt.Println(g == j)
+	fmt.Println(g != j)
+
+	// Boolean operation
+	// && = and
+	// || = or
+	// ! = opposite
+	fieldGoal := 30
+	block := 10
+	assist := 13
+
+	var mvpFieldGoal bool = fieldGoal > 20
+	var mvpBlock bool = block > 7
+	var mvpAssist bool = assist > 8
+
+	var mvp bool = mvpFieldGoal && mvpBlock && mvpAssist
+	fmt.Println("Are you the mvp :", mvp)
+
+	var mvp2 bool = mvpFieldGoal || mvpBlock || mvpAssist
+	fmt.Println("Are you the mvp :", mvp2)
+
+	// Array or list in python
+	//
+	// array used to create a set amount of variable with the same data type
+	// the difference in go, array have fixed set of data
+	// unlike python where you dont have fixed set of data
+	var kota [4]string
+	kota[0] = "Jakarta"
+	kota[1] = "Bandung"
+	kota[2] = "Bogor"
+	kota[3] = "Depok"
+
+	fmt.Println(kota)
+	fmt.Println(kota[0])
+	fmt.Println(kota[3])
+
+	// Declare and set value array at the same time
+	var angka = [4]int{
+		4, 5, 6, 7,
+	}
+	fmt.Println(angka)
+	// length array, same as lenght string
+	fmt.Println("Length array :", len(angka))
+
+	// Slice
+	// almost same like slicing in python
+	// slice is like a snapshot or a copy part of the data from array
+	// slice have 3 artibutes, pointer, length, and capacity
+	// pointer = the first data from the array
+	// length = length of the slice
+	// capacity = capacity of the slice that you get from the start of the slice(pointer)
+	// 			and end at the last of array
+	// example = array[10]
+	// slice = array[4:9]
+	// pointer = array[4]
+	// length = array[4] until array[9] so = 5, from 4 until 8, you dont count the last array in slice
+	// capacity = 7, from array[4] until array[10]
+	// if you change the value of the array, the value of the slice also changes
+	// because slice reference from array
+
+	// you can use [...] if dont know the capaticy of your array
+	var kota2 = [...]string{
+		"Jakarta",
+		"Bandung",
+		"Bogor",
+		"Depok",
+		"Tangerang",
+		"Padang",
+		"Bali",
+		"Lombok",
+		"Medan",
+	}
+
+	var slice = kota2[4:6]
+	var slice2 = kota2[2:]
+	var slice3 = kota2[:5]
+	var slice4 = kota2[:]
+	fmt.Println(slice, "\n", slice2, "\n", slice3, "\n", slice4)
+
+	// Slice function
+	// len(slice) = length of the slice
+	// cap(slice) = capacity of the slice
+	// append(slice, data) = add a new data to the end of slice, if the capacity already max, it will create a new array
+	// make([]TypeData, length, capacity) = create a new slice
+	// copy(destination, source) = copy slice from the source to the destination
+	fmt.Println("Length of the slice :", len(slice))
+	fmt.Println("Capacity of the slice :", cap(slice))
+	var slice5 = kota2[5:]
+	fmt.Println(slice5)
+
+	// since we append to the max capacity array, we create a new array
+	var slice6 = append(slice5, "Ujung Kulong")
+	fmt.Println(slice6)
+	// when we edit the new array, it doesnt effect the old array(reference array)
+	slice6[0] = "Tokyo"
+	fmt.Println(slice6)
+	// reference array
+	fmt.Println(slice5)
+	fmt.Println(kota2)
+
+	// Make slice function
+	newSlice := make([]string, 4, 6)
+	newSlice[0] = "abc"
+	newSlice[1] = "def"
+	newSlice[2] = "ghi"
+	newSlice[3] = "jkl"
+
+	fmt.Println(newSlice)
+
+	// Copy slice function
+	fromSlice := kota2[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+	copy(toSlice, fromSlice)
+	fmt.Println(toSlice)
+
+	// Array vs Slice
+	array := [3]int{1, 2, 3}    // This is array
+	array2 := [...]int{1, 2, 3} // This is also array
+	slice1 := []int{1, 2, 3}    // This is slice
+
+	fmt.Println(array)
+	fmt.Println(array2)
+	fmt.Println(slice1)
 }
